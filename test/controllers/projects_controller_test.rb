2207f7fecda80c40960072c04f0683cd59b57987
Kosmas Chatzimichalis <kosmas@mach7x.com>
2014-11-06 15:05:50 +0100

    fixed assert_routing for path with method element in it

--- test/controllers/projects_controller_test.rb
+++ test/controllers/projects_controller_test.rb
@@ -7,14 +7,13 @@ class ProjectsControllerTest < ActionController::TestCase
     assert_equal 'Runway', assigns[:action].project.name
   end
 
-  # TODO: find correct syntax for path with method element
   test 'routing' do
     assert_routing '/projects', controller: 'projects', action: 'index'
-    #assert_routing  ({ path: '/projects', method: 'post' }, { controller: 'projects' }) 
+    assert_routing  ({ path: '/projects', method: 'post' }), ({ controller: 'projects', action: 'create' })
     assert_routing '/projects/new', controller: 'projects', action: 'new'
     assert_routing '/projects/1', controller: 'projects', action: 'show', id: '1'
     assert_routing '/projects/1/edit', controller: 'projects', action: 'edit', id: '1'
-    #assert_routing ({path: '/projects/1', method: 'patch'}, controller: 'projects', action: 'update', id: '1')
-    #assert_routing ({path: '/projects/1', method: 'delete'}, controller: 'projects', action: 'destroy', id: '1')
+    assert_routing ({ path: '/projects/1', method: 'patch'}), ({ controller: 'projects', action: 'update', id: '1' })
+    assert_routing ({ path: '/projects/1', method: 'delete'}), ({ controller: 'projects', action: 'destroy', id: '1' })
   end
 end
