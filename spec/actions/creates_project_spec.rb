e26013ebdceff640dfa00ee22f0bf4fab1ca1561
Kosmas Chatzimichalis <kosmas@mach7x.com>
2014-11-23 17:39:10 +0100

    added user creation in project

--- spec/actions/creates_project_spec.rb
+++ spec/actions/creates_project_spec.rb
@@ -7,44 +7,52 @@ describe CreatesProject do
     creator.build
     expect(creator.project.name).to eq('Project Runway')
   end
-end
 
-describe 'task string parsing' do
-  let(:creator) { CreatesProject.new(name: 'Test', task_string:  task_string) }
-  let(:tasks) { creator.convert_string_to_tasks }
+  describe 'task string parsing' do
+    let(:creator) { CreatesProject.new(name: 'Test', task_string:  task_string) }
+    let(:tasks) { creator.convert_string_to_tasks }
 
-  describe 'with an empty string' do
-    let(:task_string) { '' }
-    specify { expect(tasks.size).to eq(0) }
-  end
+    describe 'with an empty string' do
+      let(:task_string) { '' }
+      specify { expect(tasks.size).to eq(0) }
+    end
 
-  describe 'with a single string' do
-    let(:task_string) { 'Start things' }
-    specify { expect(tasks.size).to eq(1) }
-    specify { expect(tasks.map(&:title)).to eq(['Start things']) }
-    specify { expect(tasks.map(&:size)).to eq([1]) }
-  end
+    describe 'with a single string' do
+      let(:task_string) { 'Start things' }
+      specify { expect(tasks.size).to eq(1) }
+      specify { expect(tasks.map(&:title)).to eq(['Start things']) }
+      specify { expect(tasks.map(&:size)).to eq([1]) }
+    end
 
-  describe 'with a single string and a size' do
-    let(:task_string) { 'Start things:3' }
-    specify { expect(tasks.size).to eq(1) }
-    specify { expect(tasks.map(&:title)).to eq(['Start things']) }
-    specify { expect(tasks.map(&:size)).to eq([3]) }
-  end
+    describe 'with a single string and a size' do
+      let(:task_string) { 'Start things:3' }
+      specify { expect(tasks.size).to eq(1) }
+      specify { expect(tasks.map(&:title)).to eq(['Start things']) }
+      specify { expect(tasks.map(&:size)).to eq([3]) }
+    end
 
-  describe 'with multiple tasks' do
-    let(:task_string) { 'Start things:3\nEnd things:2' }
-    specify { expect(tasks.size).to eq(2) }
-    specify { expect(tasks.map(&:title)).to eq(['Start things', 'End things']) }
-    specify { expect(tasks.map(&:size)).to eq([3, 2]) }
-  end
+    describe 'with multiple tasks' do
+      let(:task_string) { 'Start things:3\nEnd things:2' }
+      specify { expect(tasks.size).to eq(2) }
+      specify { expect(tasks.map(&:title)).to eq(['Start things', 'End things']) }
+      specify { expect(tasks.map(&:size)).to eq([3, 2]) }
+    end
 
-  describe 'attaching tasks to the project' do
-    let(:task_string) { 'Start things:3\nEnd things:2' }
-    it 'saves the project and tasks' do
-      creator.create
-      expect(creator.project.tasks.size).to eq(2)
-      expect(creator.project).not_to be_a_new_record
+    describe 'attaching tasks to the project' do
+      let(:task_string) { 'Start things:3\nEnd things:2' }
+      it 'saves the project and tasks' do
+        creator.create
+        expect(creator.project.tasks.size).to eq(2)
+        expect(creator.project).not_to be_a_new_record
+      end
     end
   end
+
+  it "adds users to the project" do
+    user = User.new
+    creator = CreatesProject.new(name: 'Project Runway', users: [user])
+    creator.build
+    expect(creator.project.users).to eq([user])
+  end
+
 end
